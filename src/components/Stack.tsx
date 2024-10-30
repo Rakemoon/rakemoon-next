a7801e30cb1edcb9c646cb3d7f31e95d77481779
Rakemoon <rakemoonkuzmin@gmail.com>
2024-10-30 14:44:43 +0700

    chore: use grid for stack

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -12,15 +12,16 @@ export default function Stack() {
     id={Stack.sectionId}>
     <TitleBadge>{badge}</TitleBadge>
     <Title>{title}</Title>
-    <ul className="flex gap-5 mt-2.5 flex-wrap max-w-[40rem]">
+    <ul className="grid grid-cols-8 max-md:grid-cols-4 gap-5 mt-2.5">
       {Object.values(techStack)
         .map(x => <li key={x.name} className="relative group">
-          <span className="absolute -top-1/2 left-0 right-0 text-center bg-background rounded-md border text-xs p-1 scale-0 group-hover:scale-100 transition">
+          <span className="absolute -top-1/2 left-0 right-0 text-center bg-background rounded-md border text-xs p-1 scale-0 group-hover:scale-100 transition w-max mx-auto">
             {x.name}
           </span>
           <x.icon
             width="4.5rem"
             height="4.5rem"
+            className="w-full h-full aspect-square"
           />
         </li>)
       }
